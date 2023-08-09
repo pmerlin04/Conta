806926739625dfd47bb6992cfc186a0169ffbcf2
pmerlin04 <92960530+pmerlin04@users.noreply.github.com>
2023-08-09 14:39:16 -0300

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,55 @@
+﻿using ExceptionAccount.Entities.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExceptionAccount.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; } //numero da conta
+        public string Holder { get; set; } //titular da conta
+        public double Balance { get; set; } //total da conta
+        public double WithdrawLimit { get; set; } //limite de saque
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        //método: deposito
+        //amount: quantia
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+                //nao há saldo suficiente
+            }
+            Balance -= amount;
+        }
+
+        
+        public override string ToString()
+        {
+            return "New balance: " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+                
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ExceptionAccount.Entities;
+using ExceptionAccount.Entities.Exceptions;
+using System.Globalization;
+
+namespace ExceptionAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Fazer um programa para ler os dados de uma conta bancária e depois realizar um
+            saque nesta conta bancária, mostrando o novo saldo. Um saque não pode ocorrer
+            ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de
+            saque da conta. Implemente a conta bancária conforme projeto abaixo:
+            */
+            try
+            {
+
+
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.WithDraw(amount);
+
+                Console.Write(account);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+
+        }
+    }
+}
